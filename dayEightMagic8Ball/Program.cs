dac49a641278bbe354fadf5cb2a00496b0a9301e
Your Name <you@example.com>
2016-05-16 12:00:21 -0400

    Magic 8 Ball Exercise

---
+++ dayEightMagic8Ball/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayEightMagic8Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playAgain = "N";
+            string[] responses = { "Honestly, it doesn't look good.", "You know what? Let's hope so!", "Eek, try again!",
+                                    "I think you should be optimistic.", "What? I don't even get what you're asking!", "Sure, why not?" };
+            var max = responses.Length;
+            Random random = new Random(); //instantiating a random object
+
+            do
+            {
+                Console.WriteLine("Ask me a question");
+                Console.ReadLine();
+                var selectedNum = random.Next(max);
+                Console.WriteLine(responses[selectedNum]);
+                Console.WriteLine("Do you want to play again: Y or N?");
+                playAgain = Console.ReadLine();
+                
+            } while (playAgain == "Y");
+         }
+
+            
+        }
+    }
